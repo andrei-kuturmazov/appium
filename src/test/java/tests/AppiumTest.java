d35e5e6cda343b98a00f8f5b7ebae95a953ea5e4
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-08-05 10:16:46 +0300

    adding assertions

--- src/test/java/tests/AppiumTest.java
+++ src/test/java/tests/AppiumTest.java
@@ -4,13 +4,14 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AppiumTest {
@@ -31,6 +32,16 @@ public class AppiumTest {
         try {
             driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.id("com.ruselkim.wiremocktest:id/newsButton")).click();
+            List<MobileElement> news = driver.findElements(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout"));
+            Assert.assertEquals(7, news.size());
+            news.get(0).click();
+            String linkText = driver.findElement(By.id("com.ruselkim.wiremocktest:id/details_url")).getText();
+            String author = driver.findElement(By.id("com.ruselkim.wiremocktest:id/details_author")).getText();
+            String newsTitle = driver.findElement(By.id("com.ruselkim.wiremocktest:id/toolbarTitleView")).getText();
+            Assert.assertEquals("https://lenta.ru/news/2020/08/05/sledili/", linkText);
+            Assert.assertEquals("Инкогнито", author);
+            Assert.assertEquals("СВР вела слежку за Сафроновым с осени 2019 года - Lenta.ru", newsTitle);
 
         } catch (MalformedURLException e) {
             System.out.println(e.getMessage());
